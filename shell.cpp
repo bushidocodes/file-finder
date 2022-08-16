3e71b55cfc12a76f2aa1d37d6af2962a295ed39c
Sean McBride <spmcbride1201@gmail.com>
2022-08-16 14:26:42 -0400

    refactor: shell

--- shell.cpp
+++ shell.cpp
@@ -1,46 +1,28 @@
 #define _POSIX_C_SOURCE 200809
 
-#include <errno.h>
-#include <dirent.h>
-#include <sys/types.h>
 #include <pthread.h>
 
-#include <cstdlib>
-#include <cstring>
+#include <string>
 #include <iostream>
 
 #include "con_str_vec.hpp"
 
 extern struct con_str_vec matches;
 
-void shell_dump()
-{
-	matches.foreach_del([](char *arg) -> void
-						{ std::cout << arg << "\n"; });
-}
-
 void *
 shell_main(void *argument)
 {
 	std::cout << ">> ";
-
-	char *line = nullptr;
-	std::size_t len = 0;
-	ssize_t nread = 0;
-	while ((nread = getline(&line, &len, stdin)) != -1)
+	std::string line;
+	while ((std::cin >> line))
 	{
-		if (nread >= 1 && line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
 
-		// Sort of silly to check for CRLF since I use POSIX APIs
-		if (nread >= 2 && line[nread - 2] == '\r')
-			line[nread - 2] = '\0';
-
-		if (strcmp(line, "dump") == 0)
+		if (line.compare("dump") == 0)
 		{
-			shell_dump();
+			matches.foreach_del([](char *arg) -> void
+								{ std::cout << arg << "\n"; });
 		}
-		else if (strcmp(line, "exit") == 0)
+		else if (line.compare("exit") == 0)
 		{
 			break;
 		}
@@ -52,18 +34,6 @@ shell_main(void *argument)
 
 		std::cout << ">> ";
 		std::cout.flush();
-
-		if (line != nullptr)
-		{
-			free(line);
-			line = nullptr;
-		}
-	}
-
-	if (line != nullptr)
-	{
-		free(line);
-		line = nullptr;
 	}
 
 	pthread_exit(nullptr);
